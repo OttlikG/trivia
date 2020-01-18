278a505f3032747d41080cd30d72f642195ff5ec
Gabor Ottlik <gabor.ottlik.hun@gmail.com>
2020-01-18 10:55:46 +0100

    Add snapshot test

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -10,6 +10,29 @@ describe("The test environment", function() {
   });
 });
 
-describe("Your specs...", function() {
-  // it ...
+describe("Game", function () {
+  let consoleSpy
+  let _console = console.log
+  let _random = Math.random
+
+  beforeEach(() => {
+    consoleSpy = jest.fn()
+    Math.random = jest.fn(() => 0.5)
+    console.log = consoleSpy
+  })
+
+  afterEach(() => {
+    console.log = _console
+    Math.random = _random
+  })
+
+  it('Should be the same output on every run', () => {
+    const game = new Game()
+    game.add('Chet')
+    game.add('Pat')
+
+    game.run()
+
+    expect(consoleSpy).toMatchSnapshot()
+  })
 });
