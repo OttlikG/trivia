6e46f708eba7a743e3ffc4f44c156c7767913a0b
Kálmán Tarnay <kalman@tarnay.me>
2020-01-18 14:24:36 +0100

    Add wrongAnswer tests

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -64,3 +64,58 @@ describe('didThePlayerWin', () => {
     expect(game.didPlayerWin()).toBe(false)
   })
 })
+
+describe('wrongAnswer', () => {
+  let consoleSpy
+  let _console = console.log
+
+  beforeEach(() => {
+    consoleSpy = jest.fn()
+    console.log = consoleSpy
+  })
+
+  afterEach(() => {
+    console.log = _console
+  })
+
+  it('should have the next player up for turn', () => {
+    const game = new Game();
+    game.add('Bob');
+    game.add('Alice');
+    game.add('Kate');
+    expect(game.currentPlayer).toBe(0);
+    game.wrongAnswer();
+    expect(game.currentPlayer).toBe(1);
+    game.wrongAnswer();
+    expect(game.currentPlayer).toBe(2);
+    game.wrongAnswer();
+    expect(game.currentPlayer).toBe(0);
+  })
+
+  it('should return result indicating the game should continue', () => {
+    const game = new Game();
+    const gameContinues = game.wrongAnswer();
+    expect(gameContinues).toBe(true);
+  })
+
+  it('should place the player in the penalty box', () => {
+    const game = new Game();
+    game.add('Bob')
+    consoleSpy.mockClear()
+    game.wrongAnswer();
+    expect(game.inPenaltyBox[0]).toBe(true)
+  })  
+
+  it('should print to console the expected output', () => {
+    const game = new Game();
+    game.add('Bob')
+    game.add('Clara')
+    consoleSpy.mockClear()
+    game.wrongAnswer();
+    expect(consoleSpy.mock.calls.flat()).toEqual([
+      "Question was incorrectly answered",
+      "Bob was sent to the penalty box"
+    ]);
+  })
+})
+
